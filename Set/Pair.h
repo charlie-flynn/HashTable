fd2e7bcdd089ae65a41e3087d73543a3bf3525b7
charlie-flynn <charlie.flynn.business@gmail.com>
2025-02-07 09:28:53 -0600

    removed list and operator overloads in pair

--- Set/Pair.h
+++ Set/Pair.h
@@ -10,9 +10,6 @@ public:
 	Pair<T1, T2>();
 	Pair<T1, T2>(T1 key, T2 value);
 	~Pair<T1, T2>() = default;
-
-	const bool operator==(const Pair<T1, T2>& other) const;
-	const bool operator!=(const Pair<T1, T2>& other) const;
 };
 
 template<typename T1, typename T2>
@@ -23,16 +20,4 @@ inline Pair<T1, T2>::Pair() : key(T1()), value(T2())
 template<typename T1, typename T2>
 inline Pair<T1, T2>::Pair(T1 key, T2 value) : key(key), value(value)
 {
-}
-
-template<typename T1, typename T2>
-inline const bool Pair<T1, T2>::operator==(const Pair<T1, T2>& other) const
-{
-	return value == other.value;
-}
-
-template<typename T1, typename T2>
-inline const bool Pair<T1, T2>::operator!=(const Pair<T1, T2>& other) const
-{
-	return value != other.value;
 }
